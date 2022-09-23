978464a7e9645ea0b02b9c8205c7823054b25f23
LuisSebastian28 <73673100+LuisSebastian28@users.noreply.github.com>
2022-09-23 16:22:03 -0400

    Add an alt attribute to this image.

--- src/plugins/imagery/components/ImageThumbnail.vue
+++ src/plugins/imagery/components/ImageThumbnail.vue
@@ -38,6 +38,7 @@
         <img
             class="c-thumb__image"
             :src="image.url"
+            alt="#"
         >
     </a>
     <div class="c-thumb__timestamp">{{ image.formattedTime }}</div>
